42f2eadd2ee21f4c092e8d9e53720ec7a3406510
Md Shahnawaz Alam <55729409+alamweeb@users.noreply.github.com>
2022-08-17 00:11:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
-git clone "https://github.com/EverythingSuckz/TG-FileStreamBot" tgfs
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/EverythingSuckz/TG-FileStreamBot tgfs
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO tgfs
+fi
 cd tgfs
+echo "Starting Bot...."
 python -m WebStreamer
+ 
